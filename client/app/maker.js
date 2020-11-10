f54f92c8aaa59d6d4d9686636799dd285f5ee212
myunghun0721 <hxc9272@rit.edu>
2020-11-10 17:48:18 -0500

    domo-e done

---
+++ client/app/maker.js
@@ -0,0 +1,92 @@
+const handleDomo = (e) => {
+	e.preventDefault(0);
+	
+	$("#domoMessage").animate({width:'hide'},350);
+	
+	if($("#domoName").val() == '' || $("#domoAge").val() =='' || $("#domoFood") == ''){
+		handleError("RAWR! All fields are required");
+		return false;
+	}
+	sendAjax('POST', $("#domoForm").attr("action"), $("#domoForm").serialize(),function(){
+		loadDomosFromServer();
+	});
+	
+	return false;
+};
+
+const DomoForm = (props) => {
+	return(
+	<form id="domoForm"
+		onSubmit={handleDomo}
+		name="domoForm"
+		action="/maker"
+		method="POST"
+		className="domoForm"
+	>
+	  <label htmlFor="name">Name: </label>
+	  <input id="domoName" type="text" name="name" placeholder="Domo Name"/>
+	  <label htmlFor="age">Age:</label>
+	  <input id="domoAge" type="text" name="age" placeholder="Domo Age"/>
+	  <label htmlFor="food">Food:</label>
+	  <input id="domoFood" type="text" name="food" placeholder="Domo favorite food"/>
+	  <input type="hidden" name="_csrf" value={props.csrf}/>
+	  <input className="makeDomoSubmit" type="submit" value="Make Domo"/>
+	</form>
+	);
+};
+
+const DomoList = function(props) {
+	if(props.domos.length === 0){
+		return(
+			<div className="domoList">
+				<h3 className="emptyDomo">No Domos yet</h3>
+			</div>
+		);
+	}
+	
+	const domoNodes = props.domos.map(function(domo){
+		return(
+			<div key={domo._id} className="domo">
+				<img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace"/>
+				<h3 className="domoName"> Name: {domo.name} </h3>
+				<h4 className="domoAge"> Age: {domo.age}</h4>
+				<h4 className="domoFood"> Favorite Food: {domo.food}</h4>
+			</div>
+		);
+	});
+	return(
+		<div className = "domoList">
+			{domoNodes}
+		</div>
+	);
+};
+
+const loadDomosFromServer = () =>{
+	sendAjax('GET', '/getDomos',null,(data)=>{
+		ReactDOM.render(
+			<DomoList domos={data.domos}/>,document.querySelector("#domos")
+		);
+	});
+};
+
+const setup = function(csrf) {
+	ReactDOM.render(
+		<DomoForm csrf ={csrf}/>,document.querySelector("#makeDomo")
+	);
+	
+	ReactDOM.render(
+		<DomoList domos={[]}/>, document.querySelector("#domos")
+	);
+	
+	loadDomosFromServer();
+};
+
+const getToken = () => {
+	sendAjax('GET','/getToken',null,(result)=>{
+		setup(result.csrfToken);
+	});
+};
+
+$(document).ready(function(){
+	getToken();
+});
